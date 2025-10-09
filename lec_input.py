68450edfb84dc8cffdc04252f6f7c5c75387a729
vladaaaaa1119 <vizzvlada@gmail.com>
2025-10-09 16:51:15 +0000

    my_1laba

---
+++ lec_input.py
@@ -0,0 +1,9 @@
+a = input()
+print(a)
+
+a = input("введите значение a: ")
+print(a)
+print(type(a))
+
+a = int(input("Введите згачение a: "))
+print(type(a))
---
+++ task_1.py
@@ -0,0 +1,12 @@
+print("Hello world!")
+
+a = "Маша"
+b = "Петя"
+c = "Любовь"
+print(a, "+" , b, "=" , c)
+
+a = 3
+b = 4
+print("x", "=", a, "+", b )
+x = 3 + 4
+print("x", "=", x)
