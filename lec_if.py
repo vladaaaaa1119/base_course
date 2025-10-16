947c42c3e1ed8c18e2bba6e80c19f26a3a7f6dfa
vladaaaaa1119 <vizzvlada@gmail.com>
2025-10-16 16:51:55 +0000

    11

---
+++ lec_if.py
@@ -0,0 +1,16 @@
+if 1:
+    print("hello 1")
+
+a = 3
+if a > 1:
+    print("hello 3")
+
+b = 5
+if b == 5:
+    print(f"hello 2")
+
+a = 3
+if a > 4:
+    print("hello 4")
+else:
+    print(f"hello {a}")
---
+++ task_1.py
@@ -0,0 +1,5 @@
+a = input()
+if a / 2:
+    print("четное")
+else:
+    print("нечетное")
